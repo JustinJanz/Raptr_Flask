ee9b5aeda581ad3125f52f2a03495b7e9f1030d1
Justin-Janz <Justin-Janz@users.noreply.github.com>
2020-10-08 22:22:17 +0530

    First Commit -Basic Layots

---
+++ flask blog.py
@@ -0,0 +1,44 @@
+from flask import Flask, escape, request,render_template,url_for,flash,redirect
+from forms import RegistrationForm, LoginForm
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = "b56ae9745f0cee83a96f969643d37c17"
+
+@app.route('/')
+@app.route('/home')
+
+def home():
+    return render_template('home.html')
+
+@app.route('/upload')
+def upload():
+    return render_template('upload.html', title = 'Upload')
+
+@app.route('/summary')
+def summary():
+    return render_template('summary.html', title = 'Summary')
+
+
+@app.route('/register', methods = ['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title = 'Register', form = form)
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'jjanz@gmail.com' and form.password.data =='1234':
+            flash   ('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful','danger')
+    return render_template('login.html', title = 'Login', form = form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
